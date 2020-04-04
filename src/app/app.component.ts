8d4d1e88d62c10f8117cb3d0c0b081257785e49e
Denis Putnam <denisputnam@verizon.net>
2020-04-04 19:01:07 -0400

    Completed working code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //title = 'angular-bootcamp-pw';
   private password = '';
+  private length = 0;
   private includeLetters = false;
   private includeNumbers = false;
   private includeSymbols = false;
 
   onButtonClick() {
     //console.log('Button was clicked');
-    this.password = "MY PASSWORD!!!"
+    //this.password = "MY PASSWORD!!!"
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz'
+    const symbols = '!@#$%^&*()'
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 
   onChangeUseLetters() {
@@ -28,10 +50,29 @@ export class AppComponent {
   onChangeUseSymbols() {
     this.includeSymbols = !this.includeSymbols;
   }
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
   getPassword() {
     console.log(this.includeLetters);
     console.log(this.includeNumbers);
     console.log(this.includeSymbols);
     return this.password;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  checkDisableButton() {
+    if ((this.getLength() && (this.includeLetters || this.includeSymbols || this.includeNumbers))) {
+      return false;
+    }
+    return true;
   }
 }
