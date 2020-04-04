b5b7a2d4fdc0e93bcd8733c16ab52733022a3906
Denis Putnam <denisputnam@verizon.net>
2020-04-04 16:51:11 -0400

    Added onChange logic.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,30 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //title = 'angular-bootcamp-pw';
   private password = '';
+  private includeLetters = false;
+  private includeNumbers = false;
+  private includeSymbols = false;
 
   onButtonClick() {
     //console.log('Button was clicked');
     this.password = "MY PASSWORD!!!"
   }
 
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
   getPassword() {
+    console.log(this.includeLetters);
+    console.log(this.includeNumbers);
+    console.log(this.includeSymbols);
     return this.password;
   }
 }
